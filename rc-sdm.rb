d0f43d0c77f853a61cbb66dfd239c6126adc0d28
Jamie Butler <jamie.tsh@outlook.com>
2024-01-01 16:19:24 +0800

    change dl location

--- rc-sdm.rb
+++ rc-sdm.rb
@@ -5,7 +5,7 @@ class RcSdm < Formula
     version "0.1"
   
     on_macos do
-      url "https://replaycity.tech/public/rc-sdm.tgz"
+      url "https://replaycityapi-distributionbucket41178fc6-9ze5rgy0nrnn.s3.ap-southeast-2.amazonaws.com/rc-sdm.tar.gz"
       sha256 "c44dc7eb16b373089aa5b0794d968f2c97c43e5cfaf8a4d0aeaf3aeb580c0c61"
   
       def install
